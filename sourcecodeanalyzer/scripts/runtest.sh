5773a3c9dac7d68b91bd6c7fb2481bbc56f1a2a9
marlena <xatzigrigorioum@gmail.com>
2021-06-10 12:48:16 +0300

    Add bonus implementation

---
+++ sourcecodeanalyzer/scripts/runtest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+java -jar ./target/sourcecodeanalyzer-0.0.1-SNAPSHOT-jar-with-dependencies.jar ./src/test/resources/TestClass.java regex local metrics_results csv
+
+DIFF=$(diff metrics_results.csv scripts/metrics_results_test.csv) 
+if [ "$DIFF" == "" ]; then
+    printf "Passed\n"
+
+else
+    printf "Failed\n"
+    sdiff "metrics_results.csv" "scripts/metrics_results_test.csv"
+fi

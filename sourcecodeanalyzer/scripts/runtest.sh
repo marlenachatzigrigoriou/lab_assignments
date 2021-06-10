48c483e40b9df8d67f895b658ee5c5d494878f14
marlena <xatzigrigorioum@gmail.com>
2021-06-10 13:31:52 +0300

    Change paths

--- sourcecodeanalyzer/scripts/runtest.sh
+++ sourcecodeanalyzer/scripts/runtest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-java -jar ./target/sourcecodeanalyzer-0.0.1-SNAPSHOT-jar-with-dependencies.jar ./src/test/resources/TestClass.java regex local metrics_results csv
+java -jar target/sourcecodeanalyzer-0.0.1-SNAPSHOT-jar-with-dependencies.jar ./src/test/resources/TestClass.java regex local metrics_results csv
 
 DIFF=$(diff metrics_results.csv scripts/metrics_results_test.csv) 
 if [ "$DIFF" == "" ]; then

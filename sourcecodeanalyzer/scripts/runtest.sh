3cf49602f14dca78f91f8309b44ec46beca73037
marlena <xatzigrigorioum@gmail.com>
2021-06-10 13:04:46 +0300

    Fix denied permission

--- sourcecodeanalyzer/scripts/runtest.sh
+++ sourcecodeanalyzer/scripts/runtest.sh
@@ -2,11 +2,11 @@
 
 java -jar ./target/sourcecodeanalyzer-0.0.1-SNAPSHOT-jar-with-dependencies.jar ./src/test/resources/TestClass.java regex local metrics_results csv
 
-DIFF=$(diff sourcecodeanalyzer/metrics_results.csv sourcecodeanalyzer/scripts/metrics_results_test.csv) 
+DIFF=$(diff metrics_results.csv scripts/metrics_results_test.csv) 
 if [ "$DIFF" == "" ]; then
     printf "Passed\n"
 
 else
     printf "Failed\n"
-    sdiff "sourcecodeanalyzer/metrics_results.csv" "sourcecodeanalyzer/scripts/metrics_results_test.csv"
+    sdiff "metrics_results.csv" "scripts/metrics_results_test.csv"
 fi
